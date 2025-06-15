f7063294fc845364819d0383c621f4b9e18fbf3a
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-06-16 00:39:53 +0200

    fix: foo

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import loadPkg from 'load-pkg';
 export default function () {
   const packageConfig = loadPkg.sync(this.cwd);
   return {
-    allowedMatches: ['templates'],
+    allowedMatches: ['index.yml', 'templates'],
     npmPublish: false,
     ...(!packageConfig.private && {
       deployPlugins: [
